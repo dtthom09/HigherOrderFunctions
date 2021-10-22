303bec51ccbe6ed082f9ba3c59c1433915ae6762
Dylan Thomas <dthomas@strategicmarketing.com>
2021-10-22 10:39:46 -0400

    filter map reduce simple examples

--- filterMapReduce.js
+++ filterMapReduce.js
@@ -1,68 +1,26 @@
-//just some filter/map/reduce 
+let food = [
+{ item: "taco", id: 1, value: 3},
+{ item: "burrito", id: 2, value: 8},
+{ item: "fajitas", id: 3, value: 9},
+{ item: "queso", id: 4, value: 5},
+{ item: "quesadilla", id: 5, value: 5}]
 
-var people = [
-{ name: 'bill', age: 73},
-{ name: 'mike', age: 55},
-{name: 'caleb', age:30},
-{name: 'susan', age: 60},
-{name: 'taylor', age: 30}
-]
-
-//filter 
-
-var result = people.filter(person => person.age === 30)
-console.log(result)
-
-//map
-
-var names = [] 
-for(var i=0; i < people.length; i++)
-{
-	names.push(people[i].names)
-}
-console.log(names)
+let foodFilter = food.filter(f => f.value <= 5)
+console.log('foodFilter:', foodFilter) 
+//Food items for 5 and under: 
+//[
+//  { item: 'taco', id: 1, value: 3 },
+//  { item: 'queso', id: 4, value: 5 },
+//  { item: 'quesadilla', id: 5, value: 5 }
+//]
 
 
-var age = people.map(p => p.age)
-console.log(age.sort())
+let mapFilter = food.map(f => f.item)
+console.log('mapFilter:', mapFilter)
+//Food choices: 
+//[ 'taco', 'burrito', 'fajitas', 'queso', 'quesadilla' ]
 
-var total = 0;
-
-for(var i = 0; i < age.length; i++)
-{
-	total += age[i]
-	//console.log(total)
-}
-console.log(total)
-
-var total2 = age.reduce((sum, order) => sum + order, 0)
-console.log(total2)
-
-
-var newTest = people.map(n => n.name)
-console.log(newTest)
-
-var filTest = people.filter(n => n.age > 35)
-console.log(filTest)
-
-var redTest = filTest.reduce((sum, i) => sum + i.age, 0)
-console.log(redTest)
-
-
-let products = [
-	{id: 1, price: 3},
-	{id: 2, price: 2},
-	{id: 3, price: 7},
-	{id: 4, price: 5},
-	{id: 5, price: 3},
-	{id: 6, price: 1},
-	{id: 7, price: 3}]
-
-let mapProds = products.map(a => a.price)
-console.log(mapProds)
- let sumProds = mapProds.reduce((sum, i) => sum + i, 0)
- console.log(sumProds)
- //todo use sum and i with reduce. read article and figure out reduce
-
- let finalReduce = products.reduce((sum, product) => sum + product.price, 0)
- console.log(finalReduce)
+let reduceFilter = food.reduce((sum, i) => sum + i.value, 0)
+console.log('reduceFilter:', reduceFilter)
+//food items total
+//30
